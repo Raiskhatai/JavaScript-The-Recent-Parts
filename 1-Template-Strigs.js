958da425baffc73e12bf01c80b2d96b41df26b57
Rais Khatai <raiskhatai@gmail.com>
2025-12-13 00:09:16 +0530

    first commit

---
+++ 1-Template-Strigs.js
@@ -0,0 +1,31 @@
+// Template literals JavaScript ka ek feature hai jo ES6 (2015) me aaya tha.
+// Iske 3 main kaam hote hain: (1) Multi-line strings likhna easy (2) Variable ko string ke andar directly embed karna(3) Tagged Template Literal.
+
+// (1) Multi-line strings likhna easy :
+
+// with template literals :
+// let val = `
+// hello
+// my name is
+// shahrukh
+// `;
+// console.log(val);
+
+// without template literals :
+// let val = "hello \nmy name is \nshahrukh";
+// console.log(val);
+
+// (2) Variable ko string ke andar directly embed karna :
+
+// with template literals :
+// let name = "shahrukh";
+// let greet = "hi";
+// let val = `${greet} ${name}`;
+// console.log(val);
+
+// without template literals :
+
+// let name = "shahrukh";
+// let greet = "hi";
+// let val = greet + " " + name;
+// console.log(val);
